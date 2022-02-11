39faf87c377913c99cf0e077a6dfc2b6cc81bd9d
Ankush Hegde <97220529+oshkoshank@users.noreply.github.com>
2022-02-11 15:19:59 +0000

    commmit

--- p4original.c
+++ p4original.c
@@ -14,7 +14,7 @@ int main()
   return 0;
 }
 
-int input_array_size();
+int input_array_size()
 {
   int a;
   printf("Enter the array size: ");
@@ -34,7 +34,17 @@ void input_array(int n, int a[n])
 int sum_composite_numbers(int n, int a[n])
 {
   int sum = 0;
-   
+  for(int i = 0; i<n; i++)
+  {
+      for(int j=2;j<a[i];j++)
+      {
+          if(a[i]%j==0)
+          {
+              sum = sum + a[i];
+              break;
+          }
+      }
+  }
   return sum;
 }
 
