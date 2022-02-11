0aff831bfd91498aee3eba0fb60b418bdd6282f2
Ankush Hegde <97220529+oshkoshank@users.noreply.github.com>
2022-02-11 15:49:15 +0000

    commmit but change the p6

--- p6original.c
+++ p6original.c
@@ -1,37 +1,41 @@
 #include<stdio.h>
-#include<string.h>
 
-void input_string();
-char str_reverse();
-void output();
+void input_string(char *a)
+{
+  printf("Enter the string to be reversed: ");
+  scanf("%s",a);
+}
+char str_reverse(char *a)
+{
+  int temp;
+  for(int i = 0; a[i]!='\0';i++)
+  for(int j = 0; j < i/2; j++ )
+    {
+      temp = a[j];
+      a[j] = a[i-1-j];
+      a[i-1-j] = temp;
+    }
+    return a;
+}
+void output()
+{
+    prin
+}
 
-int main ()
+/*-------------------------------------------------*/
+
+int main()
 {
   char a[50];
   input_string(a);
-  char rev[50] = str_reverse(a);
+  char rev[50] ={ str_reverse(a)};
+  output(a,rev);
   return 0;
 }
 
-void input_string(char a)
-{
-  printf("Enter the string to be reversed: ");
-  scanf("%s"&a);
-}
+/*-------------------------------------------------*/
 
-char str_reverse( char a)
-{
-  int l,tp;
-  l=strlen(a);
-  for(int i = 0; i < l/2; i++)
-    {
-      tp = a[i];
-      a[i] = a[l-i-1];
-      a[l-i-1] = tp;
-    }  
-}
 
-void output(char a, char *reversea)
-{
-  printf("The reverse of %s is %s ",a,reversea);
-}
+
+/*-------------------------------------------------*/
+
