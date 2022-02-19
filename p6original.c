fe1cd39c2284e7c49480a4e71b074c1aa49d3852
Ankush Hegde <97220529+oshkoshank@users.noreply.github.com>
2022-02-19 11:52:09 +0000

    commit

--- p6original.c
+++ p6original.c
@@ -1,41 +1,46 @@
 #include<stdio.h>
+void input_string();
+void str_reverse();
+void output();
 
-void input_string(char *a)
-{
-  printf("Enter the string to be reversed: ");
-  scanf("%s",a);
-}
-char str_reverse(char *a)
-{
-  int temp;
-  for(int i = 0; a[i]!='\0';i++)
-  for(int j = 0; j < i/2; j++ )
-    {
-      temp = a[j];
-      a[j] = a[i-1-j];
-      a[i-1-j] = temp;
-    }
-    return a;
-}
-void output()
-{
-    prin
-}
-
-/*-------------------------------------------------*/
+/*-----------------------------------------------------*/
 
 int main()
 {
-  char a[50];
-  input_string(a);
-  char rev[50] ={ str_reverse(a)};
-  output(a,rev);
-  return 0;
+    char a[50],b[50];
+    input_string(a);
+    str_reverse(a,b);
+    output(a,b);
+    return 0;
 }
 
-/*-------------------------------------------------*/
+/*-----------------------------------------------------*/
 
+void input_string(char *a)
+{
+    printf("Enter the strig to be reversed: ");
+    scanf("%s",a);
+}
 
+/*-----------------------------------------------------*/
 
-/*-------------------------------------------------*/
+void str_reverse( char *a, char *b)
+{
+    int n,i;
+    for( i = 0; a[i]!='\0';++i);
+    n=i;
+    int l = n-1;
+    for(i = 0; i < n; i++)
+    {
+        b[i]=a[l];
+        l--;
+    }
+    b[i]='\0';
+}
 
+/*-----------------------------------------------------*/
+
+void output(char *a, char *b)
+{
+    printf("The reverse of the string %s is %s ",a,b);
+}
